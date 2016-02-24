519367716497916ee94473e55e5b66057681ae84
Tony Meissner <e_meiss@hotmail.com>
2016-02-24 21:10:59 +1030

    Update #1
    Have included Scatter Chart

--- javascripts/cbrChart.js
+++ javascripts/cbrChart.js
@@ -11,8 +11,10 @@ var app = angular.module('cbrChart',[]);
 
 app.controller('cbrChartController', ['$scope', function ($scope) {
 
-    $scope.selectedStat="Turn";
+    $scope.xStat="Turn";
+    $scope.yStat="Population";
     $scope.selectedCiv="Australia";
+    $scope.ChartType="lineWithFocusChart";
     
     $scope.data = [];
     $scope.options = {
@@ -49,13 +51,14 @@ app.controller('cbrChartController', ['$scope', function ($scope) {
             }
         };
 
-    this.updateData = function(xVar,yVar,Civ,cbrData) {
+    this.updateData = function(ChartType,xVar,yVar,Civ,cbrData) {
         
         $scope.data = cbrUtilities.prepareData(cbrUtilities.covertDataToXY(cbrData,xVar,yVar),Civ,$scope.CivColors);
         
+        if (ChartType=="lineWithFocusChart") {
         $scope.options = {
             chart: {
-                type: 'lineWithFocusChart',
+                type: ChartType, //'lineWithFocusChart'
                 height: 700,
                 margin : {
                     top: 50,
@@ -64,12 +67,6 @@ app.controller('cbrChartController', ['$scope', function ($scope) {
                     left: 100
                 },
                 useInteractiveGuideline: true,
-                dispatch: {
-                    stateChange: function(e){ console.log("stateChange"); },
-                    changeState: function(e){ console.log("changeState"); },
-                    tooltipShow: function(e){ console.log("tooltipShow"); },
-                    tooltipHide: function(e){ console.log("tooltipHide"); }
-                },
                 xAxis: {
                     axisLabel: xVar
                 },
@@ -80,14 +77,62 @@ app.controller('cbrChartController', ['$scope', function ($scope) {
                     }
                 },
                 x2Axis: {},
-                y2Axis: {},
-                callback: function(chart){
-                    console.log("!!! lineChart callback !!!");
-                }
+                y2Axis: {}
             }
         };
+      }
+        if (ChartType=="scatterChart") {
+            $scope.options= {
+                chart: {
+                type: ChartType, //'scatterChart'
+                height: 700,
+                margin : {
+                    top: 50,
+                    right: 100,
+                    bottom: 50,
+                    left: 100
+                },
+                showDistX: true,
+                showDistY: true,
+                duration: 350,
+                xAxis: {
+                    axisLabel: xVar,
+                    tickFormat: function(d){
+                        return d3.format(',.2f')(d);
+                    }
+                },
+                yAxis: {
+                    axisLabel: yVar,
+                    tickFormat: function(d){
+                        return d3.format(',.2f')(d);
+                    }
+                },
+                zoom: {
+                  enabled: true,
+                  scaleExtent: [
+                    1,
+                    100
+                  ],
+                  useFixedDomain: true,
+                  useNiceScale: false,
+                  horizontalOff: false,
+                  verticalOff: false,
+                  unzoomEventType: "dblclick.zoom"
+                }
+              }
+        }
+    };
     }
+    
 }]);  
     
+
+app.controller('cbrScatterController', ['$scope', function ($scope) {
+
+
+
+    
+}]);
+
     
 })();
